f2d77299bd2d5bde881e26df50ba611dbce4a2f8
LenaraH <112811502+LenaraH@users.noreply.github.com>
2024-08-05 21:46:45 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Hello, World!</h1>
+    <h3>Study React</h3>
+    <p>Day 1, ReactDOM, render()</p>
+  </div>,
+  document.getElementById("root")
+);
